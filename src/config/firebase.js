d9cc4cd35610e0bc16ff614dd7d079d8b71faaf9
Erlan <erlanamanbekov797@gmail.com>
2023-04-02 17:36:27 +0300

    add google authentication

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyD81s9zPx6c012TAWqL1Ubx8ttaPtmf0wU",
   authDomain: "first-project-e17d3.firebaseapp.com",
@@ -11,3 +11,5 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
